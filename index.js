82fbe3dcd6a5efae71cf7f9e002525b7aee23753
NickNocc <haretakis4@gmail.com>
2022-01-09 11:47:23 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const inquirer = require(`inquirer`);
 const Employee = require(`./utils/employee`);
 const Department = require(`./utils/department`);
 const Role = require(`./utils/role`);
+const mysql = require(`mysql2`);
