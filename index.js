876a7dbf3d33f5eeecf9a03b6fa625678885c083
NickNocc <haretakis4@gmail.com>
2022-01-09 11:43:08 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ const inquirer = require(`inquirer`);
 const Employee = require(`./utils/employee`);
 const Department = require(`./utils/department`);
 const Role = require(`./utils/role`);
-const let = "'"
